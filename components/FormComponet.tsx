a112bb178b81ea9f65020f24e6e51dc7a7d296d7
Victor Pinheiro Mello <victormello7003@hotmail.com>
2025-08-27 12:25:00 -0300

    feat: create FormCOmponent final

--- components/FormComponet.tsx
+++ components/FormComponet.tsx
@@ -12,13 +12,21 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const formSchema = Z.object({
   username: Z.string()
@@ -26,6 +34,11 @@ const formSchema = Z.object({
       message: "Username must be at least 2 characters.",
     })
     .max(50),
+  whatsapp: Z.string(),
+  email: Z.email(),
+  objective: Z.string().min(1, {
+    message: "Nome de usuário deve ter pelo menos 3 caracteres",
+  }),
 });
 
 export function FormComponent() {
@@ -35,6 +48,9 @@ export function FormComponent() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      whatsapp: "",
+      email: "",
+      objective: "",
     },
   });
 
@@ -58,25 +74,110 @@ export function FormComponent() {
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="p-4 backdrop-blur-md"
+          className="flex flex-col justify-center gap-4 p-4 backdrop-blur-sm rounded-4xl w-3xl h-[30rem]"
         >
           <FormField
             control={form.control}
             name="username"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
                 <FormControl>
-                  <Input placeholder="shacn" {...field} />
+                  <Input
+                    type="text"
+                    className="h-16 !text-2xl border-0 !bg-primary-foreground !text-primary"
+                    placeholder="Nome*"
+                    required
+                    {...field}
+                  />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <FormField
+            control={form.control}
+            name="whatsapp"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="tel"
+                    className="h-16 !text-2xl border-0 !bg-primary-foreground !text-primary"
+                    placeholder="WhatsApp*"
+                    required
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="email"
+                    className="h-16 !text-2xl border-0 !bg-primary-foreground !text-primary"
+                    placeholder="Email*"
+                    required
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="objective"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-full !h-16 !text-2xl border-0 !bg-primary-foreground !text-primary">
+                      <SelectValue placeholder="Objetivo Principal*" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem
+                        className="w-full !h-16 !text-2xl border-0 !bg-primary-foreground !text-primary hover:!bg-primary-foreground/80"
+                        value="perder-peso"
+                      >
+                        Perder Peso
+                      </SelectItem>
+                      <SelectItem
+                        className="w-full !h-16 !text-2xl border-0 !bg-primary-foreground !text-primary hover:!bg-primary-foreground/80"
+                        value="ganhar-massa"
+                      >
+                        Ganhar Massa Muscular
+                      </SelectItem>
+                      <SelectItem
+                        className="w-full !h-16 !text-2xl border-0 !bg-primary-foreground !text-primary hover:!bg-primary-foreground/80"
+                        value="reeducacao"
+                      >
+                        Reeducação Alimentar
+                      </SelectItem>
+                      <SelectItem
+                        className="w-full !h-16 !text-2xl border-0 !bg-primary-foreground !text-primary hover:!bg-primary-foreground/80"
+                        value="melhora-saude"
+                      >
+                        Melhoria da Saúde
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button
+            className="h-16 !text-2xl border-0 !bg-primary-foreground !text-primary"
+            type="submit"
+          >
+            Submit
+          </Button>
         </form>
       </Form>
     </motion.section>

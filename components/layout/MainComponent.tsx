44873e5a8cbca2d21f5fcdbc940eb4de26127958
Victor Pinheiro Mello <victormello7003@hotmail.com>
2025-09-01 09:13:47 -0300

    refactor: create organized folder structure for components
    - Add components/ui/ for reusable UI components
    - Add components/layout/ for Header, Footer, FixedThemeToggle
    - Add components/sections/ for page sections
    - Add components/forms/ for form components
    - Add providers/ folder for global providers

---
+++ components/layout/MainComponent.tsx
@@ -0,0 +1,25 @@
+import { HeroSection } from "@/components/sections/HeroSection";
+import { FormComponent } from "@/components/forms/FormComponet";
+import { MethodComponent } from "@/components/sections/MethodComponent";
+import { ResultsComponent } from "@/components/sections/ResultsComponent";
+import { DifferentialComponent } from "@/components/sections/DifferentialComponent";
+import { InfosComponent } from "@/components/sections/InfosComponent";
+import { Photos } from "@/components/sections/Photos";
+import { FAQComponent } from "@/components/sections/FAQComponent";
+import { ConsultationForm } from "@/components/forms/ConsultationForm";
+
+export function MainComponent() {
+  return (
+    <main>
+      <HeroSection />
+      <FormComponent />
+      <MethodComponent />
+      <ResultsComponent />
+      <DifferentialComponent />
+      <InfosComponent />
+      <Photos />
+      <FAQComponent />
+      <ConsultationForm />
+    </main>
+  );
+}
